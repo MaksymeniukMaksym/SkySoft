41ff5fab275d1ef6ef5796bd20a577bd985c05c3
Maksym <MaksymMaksymeniuk@gmail.com>
2019-04-19 19:08:55 +0300

    Done

--- arr/rec_funk.js
+++ arr/rec_funk.js
@@ -1,55 +1,55 @@
-(function(){
-    var arr = [
-        1,
-        [10, 11, 22, 2],
-        [40, 1, 2, 3],
-      3,
-      [],
-      4,
-      5,
-      [
-          [40, 1, 2, 3],    
-          [
-        2, 
-        3, 
-        [40, 1, 2, 3]
-        ],
-      ],
-      7,
-      8
-    ];
 
-    // Є вхідний массив arr, який включає в себе числа, або вкладені масиви.
+  var arr = [
+      1,
+      [10, 11, 22, 2],
+      [40, 1, 2, 3],
+    3,
+    [],
+    4,
+    5,
+    [
+        [40, 1, 2, 3],    
+        [
+      2, 
+      3, 
+      [40, 1, 2, 3]
+      ],
+    ],
+    7,
+    8
+  ];
+
+  // Є вхідний массив arr, який включає в себе числа, або вкладені масиви.
 // 1. Потрібно розоргнути масив в одномірний ( [1,[2,3],[4, 5]] => [1,2,3,4,5])
 // 2. В числах які >10 залиш лише останній розряд.
 // 3. Всі елементи з значенням - 0 заміни на два елементи які в сумі дають 10 (8 і 2). ( [1,0,2] => [1,3,7,2])
 // 4. Перетвори масив в стрічку в якій елементи розділені через "-". ( [1,2,3,]  => "1-2-3")
 
-    //1 
-    function flatDeep(arr, i = 0) {
-      
-      if(i == arr.length)  return arr;
+  //1 
+  function flatDeep(arr, i = 0) {
+    
+    if(i == arr.length)  return arr;
 
-        if(Array.isArray(arr[i])){
-           arr.splice(i,1, ...arr[i])
-           i--;
-        }
-        return flatDeep(arr, ++i);     
-       }
-       arr = flatDeep(arr);
+      if(Array.isArray(arr[i])){
+         arr.splice(i,1, ...arr[i])
+         i--;
+      }
+      return flatDeep(arr, ++i);     
+     }
+     arr = flatDeep(arr);
 
-       //2
-       arr = arr.map(function(num) {
-        return num % 10;
-       });
-       
-       //3
-       for(let i = 0; i < arr.length; i++){
-         if(arr[i] === 0){
-          arr.splice(i, 1, 5 ,5);
-         }
+     //2
+     arr = arr.map(function(num) {
+      return num % 10;
+     });
+     
+     //3
+     for(let i = 0; i < arr.length; i++){
+       if(arr[i] === 0){
+        arr.splice(i, 1, 5 ,5);
        }
-    //4
-    arr = arr.join('-');
-    console.log(arr);
-    })();
+     }
+  //4
+  arr = arr.join('-');
+  console.log(arr);
+  
