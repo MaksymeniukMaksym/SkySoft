3432e69cba96c205bdc8aca5185d48fbbba5f379
Maksym <MaksymMaksymeniuk@gmail.com>
2019-04-22 17:21:19 +0300

    Finish

--- join/script.js
+++ join/script.js
@@ -1,19 +1,24 @@
 
 var arr = [1,[[[2]]],2,[],3,'abc',[4,[7, [34]],5,[],6]];
-Array.prototype.join = function(str) {
-                    let result = "";
-                    function innerFunction(innerArr) {
-                        for (let element of innerArr) {
-                            if (!Array.isArray(element)) {
-                                result += element + str;
-                            } else {
-                                innerFunction(element);
-                            }
-                        }
-                        
-                        return result.slice(0,-1);
-                    }
-                    return innerFunction(arr) ;
-                };
+var arr1 = ['abc',[4,[7, [34]]]];
+var arr2 = [1,5,[],6];
 
-console.log(arr.join('-')); 
+Array.prototype.join = function(str) {
+  let result = "";
+  function innerFunction(innerArr) {
+    for (let element of innerArr) {
+      if (!Array.isArray(element)) {
+        result += element + str;
+      } else {
+        innerFunction(element);
+      }
+    }
+
+    return result.slice(0,-1);
+  }
+  return innerFunction(this) ;
+};
+
+console.log(arr.join('-'));
+console.log(arr1.join('-'));
+console.log(arr2.join('-'));
