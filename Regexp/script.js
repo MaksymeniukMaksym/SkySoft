b733206067630d5479422adbab439b450fba0439
Maksym <MaksymMaksymeniuk@gmail.com>
2019-04-30 14:35:54 +0300

    1/3(refactoring)

--- Regexp/script.js
+++ Regexp/script.js
@@ -1,27 +1,51 @@
-
-const check_link = (() => {
+//<схема>:[//[<логин>[:<пароль>]@]<хост>[:<порт>]][/<URL‐путь>][?<параметры>][#<якорь>]
+//"https://Maks:3226@Maks.ru:27000/papka/next?параметр_1=значение_1&параметр_2=значение_2#_URl_";
+const check_url = (() => {
     const input = document.getElementById("link");
     return () => {
-        let regexp = {
-            Password: /:\w*(?=@)/,
-            Login: /\w*@/,
-            Port: /\:+\d+/,
-            Protocol: /\b\w*:\/\//,
-            Host: /\b\w*\.\w*(\.\w*)?/
-        },
             url = input.value,
             text = '',
             tmp = null;
 
-        for (let key in regexp) {
-            tmp = url.match(regexp[key]);
-            if (tmp !== null) {
-                tmp = tmp[0];
-                url = url.replace(tmp, '');
-                tmp = tmp.replace(/:|@/,'');
-                text += `${key}: ${tmp}<br/>`;
-            }
-        }
-        elem.innerHTML = text || 'Please enter the valid url';
+            let regexp = /(?:([^\:]*)\:\/\/)?(?:([^\:\@]*)(?:\:([^\@]*))?\@)?(?:([^\/\:]*)\.(?=[^\.\/\:]*\.[^\.\/\:]*))?([^\.\/\:]*)(?:\.([^\/\.\:]*))?(?:\:([0-9]*))?(\/[^\?#]*(?=.*?\/)\/)?([^\?#]*)?(?:\?([^#]*))?(?:#(.*))?/;
+    
+                text = '',
+                tmp = null;
+                tmp = url.match(regexp);
+    
+                let name = {      
+                    URL: tmp[0],
+                    Protocol: tmp[1],
+                    Login: tmp[2],
+                    Password: tmp[3],
+                    Host: (tmp[4] !== undefined ? (tmp[4]+'.'):'')+(tmp[5] !== undefined ? (tmp[5]):'')+(tmp[6] !== undefined ? ('.'+tmp[6]):''),
+                    Port: tmp[7],    
+                    Query: tmp[10],
+                }            
+            
+                Object.keys(name).forEach(key => { text += (key + ":"+name[key] + "<br/>") });
+        elem.innerHTML = name.Protocol !== undefined ? text : 'Please enter the valid url';
+    }
+})();
+
+const check_folder = (() => {
+    const input = document.getElementById("file");
+    return () => {
+            file_way = input.value,
+            text = '',
+            tmp = null;
+
+            let regexp = / /;
+    
+                text = '',
+                tmp = null;
+                tmp = file_way.match(regexp);
+    
+                let name = {      
+            
+                }            
+            
+                Object.keys(name).forEach(key => { text += (key + ":"+name[key] + "<br/>") });
+        folder.innerHTML = name.Foo !== undefined ? text : 'Please enter the valid url';
     }
 })();
