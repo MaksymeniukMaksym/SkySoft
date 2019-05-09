1264c29c203432a3be43f15616f075f972bed0c1
Maksym <MaksymMaksymeniuk@gmail.com>
2019-05-09 21:09:06 +0300

    Don`t work.

--- Node.js/Chapter_3/Task_3/server.js
+++ Node.js/Chapter_3/Task_3/server.js
@@ -1,27 +1,30 @@
 const http = require("http");
 const fs = require("fs");
- 
+ 
 http.createServer(function(request, response){
-     
-    console.log(`Aдрес: ${request.url}`);
-    if(request.url.startsWith("/root/")){
-         
-       
-        const filePath = request.url.substr(1);
-        fs.readFile(filePath, function(error, data){
-                 
-            if(error){
-                     
-                response.statusCode = 404;
-                response.end("Not found!");
-            }   
-            else{
-                response.setHeader("Content-Type", "script/javascript");
-                response.end(data);
-            }
-        })
-    }
-    else{
-        response.end("Hello World!");
-    }
+     
+    console.log(process.argv[2])
+
+    console.log(`Aдрес: ${request.url}`);
+   
+    if(process.argv[2] !== undefined)  {
+        response.writeHead(200, {"Content-Type" : "application/form-data"});
+        fs.createReadStream(process.argv[2].substr(1)).pipe(response);
+    }
+          filePath = request.url.substr(1);
+         
+        fs.readFile(filePath, function(error, data){
+                 
+            if(error){
+                     
+                response.statusCode = 404;
+                response.end("Not found!");
+            }   
+            else{
+                response.writeHead(200, {"Content-Type" : "application/form-data"});
+                response.end(data);
+            }
+        })
+    
+   
 }).listen(3000);
