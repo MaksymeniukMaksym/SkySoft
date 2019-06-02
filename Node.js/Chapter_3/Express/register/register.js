d676f9b7f2530857370f73f7bd79e89ba6fa98c1
Maksym <MaksymMaksymeniuk@gmail.com>
2019-06-02 17:07:27 +0300

    Joi validator add

--- Node.js/Chapter_3/Express/register/register.js
+++ Node.js/Chapter_3/Express/register/register.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const Joi = require('@hapi/joi');
 
 const app = express();
 
@@ -10,11 +11,23 @@ app.get("/register", urlencodedParser, function (request, response) {
 
 });
 
+const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    birthyear: Joi.number().integer().min(1900).max(2019),
+    
+}).with('username', 'birthyear').without('password', 'access_token');
+
 app.post("/register", urlencodedParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     console.log(request.body);
-    response.send(`${request.body.userName} - ${request.body.userAge}`);
+    const result = Joi.validate({ username: request.body.userName, birthyear: request.body.userbirsday}, schema,function (err, value) {
+        if(err){
+            response.send("Invalid")
+        }
 
+     });
+    response.send(`Name: ${request.body.userName} <br> Birthyear: ${request.body.userbirsday}`);
+    response.send();
 });
 
 app.get("/", function (request, response) {
